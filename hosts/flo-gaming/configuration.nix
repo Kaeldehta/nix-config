5387c1d378b6e2691a8db2fdedce74591976168e
Kaeldehta <flo@visiolab.io>
2025-10-03 17:22:33 +0200

    fix pnpm binaries not working

--- hosts/flo-gaming/configuration.nix
+++ hosts/flo-gaming/configuration.nix
@@ -83,6 +83,8 @@
     enable = true;
     enable32Bit = true;
   };
+
+  programs.nix-ld.enable = true;
   
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
